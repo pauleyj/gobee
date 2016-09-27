7c1143134245cefc105df40ffdd20a5793c3ebcb
John Pauley <pauleyj@gmail.com>
2016-09-27 18:43:30 -0400

    fix test

--- xbee_test.go
+++ xbee_test.go
@@ -35,7 +35,7 @@ func (r *Receiver) Receive(f rx.Frame) error {
 }
 
 func validateAT(t *testing.T, f *rx.AT) {
-	if f.ID != 1 {
+	if f.ID() != 1 {
 		t.Errorf("Expected FrameId: 0x01, but got 0x%02x", f.ID)
 	}
 }
